9b80001b86781efd7fb9065e8957b41bfd0084e9
Mateus <matbessa12@gmail.com>
2024-12-10 12:27:29 -0300

    First commit

---
+++ source/path/path_reference.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+
+def get_root_folder_path() -> Path:
+    return Path(__file__).parent.parent.parent
+
+
+def get_source_folder_path() -> Path:
+    return Path(__file__).parent.parent
+
+
+def get_scrapper_folder_path() -> Path:
+    return Path(get_source_folder_path(), 'core_scrapper')
+
+
+def get_gmail_folder_path() -> Path:
+    return Path(get_source_folder_path(), 'gmail_analysis')
+
+
+def get_credentials_path() -> Path:
+    return Path(get_gmail_folder_path(), 'credentials.json')
+
+def get_data_folder_path() -> Path:
+    return Path(get_source_folder_path(), 'data')
+
+
+def main():
+    folder = get_scrapper_folder_path()
+    print(folder)
+    return
+
+
+if __name__ == '__main__':
+    main()
