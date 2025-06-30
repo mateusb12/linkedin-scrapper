7db22c435132e8995e9ad0bf472b56cc16aefe47
Mateus <matbessa12@gmail.com>
2025-06-30 15:46:22 -0300

    feat: enable CORS support in Flask application

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 from controllers.job_curls import fetch_jobs_bp
 from database.database_connection import create_db_and_tables
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(fetch_jobs_bp)
 
