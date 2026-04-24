bfd6677eb2267e5fc67c2a1c8b966b05fd19086e
mateusb12 <matbessa12@gmail.com>
2026-04-24 12:42:45 -0300

    feat(setup): initialize React project with Vite, TypeScript, and Tailwind CSS

---
+++ new-frontend/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+    return (
+        <div className="min-h-screen bg-black flex items-center justify-center">
+            <h1 className="text-4xl text-green-400 font-bold">
+                Tailwind funcionando 🚀
+            </h1>
+        </div>
+    );
+}
